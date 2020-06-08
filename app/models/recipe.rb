3c92ffd0c3aa910ee3ed0d0b170d0289fbea06e4
chriskay25 <chriskay25@gmail.com>
2020-06-08 11:29:14 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,16 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  # {
+  #   :recipe => {
+  #     :title,
+  #     :ingredients_attributes => {
+  #       "0" => {
+  #         :name,
+  #         :quantity
+  #       }
+  #     }
+  #   }
+  # }
 end
